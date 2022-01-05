b3bca106681de9e923520611688ddb11df3a12e1
Alain Osifre <alain.osifre.aosweb@gmail.com>
2022-01-05 18:12:48 +0100

    changement dans la page index

---
+++ js/button-copy.js
@@ -0,0 +1,17 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
+
+function copyToClickBoard(){
+    var content = document.getElementById('textArea').innerHTML;
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+ 
+}
